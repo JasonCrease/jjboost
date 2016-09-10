1a2ff1728333501ffdcf4c4864b9911b4a0eb8dd
Jason Crease <jason.crease@red-gate.com>
2016-09-10 18:38:07 +0100

    random number tests

--- src/test/java/GbTreeTests.java
+++ src/test/java/GbTreeTests.java
@@ -9,19 +9,31 @@ public class GbTreeTests {
     Random random = new Random(2);
 
     @Test
-    public void Test1() throws Exception {
+    public void RandomNumbers() throws Exception {
 
+        double[][] dataXs = DataSets.getRandomXs(10, 200000);
+        double[]   dataYs = DataSets.getRandomYs(200000);
+
+        GBTrees gbTrees = (new GBTrees.GBTreesBuilder()).setMaxTrees(50).setMaxTreeDepth(2).build();
+        gbTrees.train(dataXs, dataYs);
+    }
+
+    @Test
+    public void HouseData() throws Exception {
         double[][] dataXs = DataSets.getHousePriceData()._xs;
         double[]   dataYs = DataSets.getHousePriceData()._ys;
 
-        GBTrees gbTrees = (new GBTrees.GBTreesBuilder()).build();
+        GBTrees gbTrees = (new GBTrees.GBTreesBuilder()).setMaxTrees(50).setMaxTreeDepth(3).build();
         gbTrees.train(dataXs, dataYs);
+    }
 
-        double[] predVector = new double[dataYs.length];
-        for(int col = 0; col < dataYs.length; col++)
-            predVector[col] = random.nextDouble() * 200;
+    @Test
+    public void IrisData() throws Exception {
+        double[][] dataXs = DataSets.getIrisData()._xs;
+        double[]   dataYs = DataSets.getIrisData()._ys;
 
-        gbTrees.predict(predVector);
+        GBTrees gbTrees = (new GBTrees.GBTreesBuilder()).setMaxTrees(25).setMaxTreeDepth(3).build();
+        gbTrees.train(dataXs, dataYs);
     }
 
     @Test
@@ -36,7 +48,7 @@ public class GbTreeTests {
         for(int col = 0; col < numCols; col++)
             predVector[col] = random.nextDouble() * 200;
 
-        GBTrees gbTrees = (new GBTrees.GBTreesBuilder()).build();
+        GBTrees gbTrees = (new GBTrees.GBTreesBuilder()).setMaxTrees(10).build();
         gbTrees.train(dataXs, dataYs);
         gbTrees.predict(predVector);
     }
